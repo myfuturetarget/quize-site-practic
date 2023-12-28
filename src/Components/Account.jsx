448bdf0b104cdbcb20e3dcb0a33431aac6f02d97
Hejbul_rahman <tasinahamed503@gmail.com>
2023-12-28 11:19:26 +0600

    responsive done

--- src/Components/Account.jsx
+++ src/Components/Account.jsx
@@ -14,19 +14,36 @@ const Account = () => {
   return (
     <div>
       {currentUser ? (
-        <div className={classes.main_account}>
-          <a href="#" title="Your Account" className={classes.account}>
-            <i
-              className={`fa-regular fa-circle-user ${classes.account_icon}`}
-            ></i>
-            <span className={classes.userSpan}>{currentUser.displayName}</span>
-          </a>
+        <div>
+          <div className={classes.main_account}>
+            <a href="#" title="Your Account" className={classes.account}>
+              <i
+                className={`fa-regular fa-circle-user ${classes.account_icon}`}
+              ></i>
+              <span className={classes.userSpan}>
+                {currentUser.displayName}
+              </span>
+            </a>
 
-          <a title="logout" onClick={logoutFunc}>
-            <i
-              className={`fa-solid fa-right-from-bracket ${classes.account_icon2}`}
-            ></i>
-          </a>
+            <a title="logout" onClick={logoutFunc}>
+              <i
+                className={`fa-solid fa-right-from-bracket ${classes.account_icon2}`}
+              ></i>
+            </a>
+          </div>
+          {/* <div className={classes.sidebar}>
+            <a href="#" title="Your Account" className={classes.myAccount}>
+              <i
+                className={`fa-regular fa-circle-user ${classes.my_account_icon}`}
+              ></i>
+              <span className={classes.username}>
+                {currentUser.displayName}
+              </span>
+            </a>
+            <a className={classes.smallLogout} onClick={logoutFunc}>
+              Logout
+            </a>
+          </div> */}
         </div>
       ) : (
         <>
